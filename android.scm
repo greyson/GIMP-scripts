2cff010d9f1186138d98e5690b6d4d25af92a847
Greyson Fischer <greyson@foosoft.us>
2011-07-10 02:37:38 -0400

    Save all versions of a 9-patch

--- android.scm
+++ android.scm
@@ -139,3 +139,72 @@
   )
 (script-fu-menu-register "android-render-nine-patch" "<Image>/Image/Android")
 
+; Get the last element (as in, remove path from listified filename)
+(define (last-element lis)
+  (cond ((null? (cdr lis)) (car lis))
+        (else (last-element (cdr lis))) ))
+
+(define (morph-filename orig-name new-extension)
+  (let* ((buffer (vector "" "" "")))
+    (if (re-match "^(.*)[.]([^.]+)$" orig-name buffer)
+      (string-append (substring orig-name 0 (car (vector-ref buffer 2))) new-extension)) ))
+
+(define (filename-extension filename new-extension)
+  (morph-filename (last-element (strbreakup filename "/")) new-extension) )
+
+(define (android-save-scaled-9patch theImage theDrawable theDirectory scale-factor)
+  (let*
+    ((new-xcf-file (car (gimp-temp-name "xcf")))
+     (result-png-file (string-append theDirectory "/"
+                                     (filename-extension (car (gimp-image-get-filename theImage)) "9.png")))
+     (width (car (gimp-image-width theImage)))
+     (height (car (gimp-image-height theImage)))
+     (new-image 0)
+     (result-layer 0) )
+    (gimp-xcf-save 0 theImage theDrawable new-xcf-file new-xcf-file)
+    (set! new-image (car (gimp-xcf-load 0 new-xcf-file new-xcf-file)))
+    (gimp-image-scale-full new-image
+                           (* scale-factor width)
+                           (* scale-factor height)
+                           INTERPOLATION-CUBIC)
+    (android-render-nine-patch new-image)
+    (set! result-layer (car (gimp-image-merge-visible-layers new-image EXPAND-AS-NECESSARY)))
+    (file-png-save2 1
+                    new-image
+                    result-layer
+                    result-png-file
+                    result-png-file
+                    0 9 0 0 0 0 0 0 0 )
+    (gimp-image-delete new-image)
+  ))
+
+
+(define (android-save-all-resolutions theImage theDrawable theDirectory)
+  (let*
+    ( (resolution (car (gimp-image-get-resolution theImage))) )
+    (android-save-scaled-9patch theImage theDrawable
+                                (string-append theDirectory "/drawable-ldpi")
+                                (/ 120 resolution))
+    (android-save-scaled-9patch theImage theDrawable
+                                (string-append theDirectory "/drawable-mdpi")
+                                (/ 160 resolution))
+    (android-save-scaled-9patch theImage theDrawable
+                                (string-append theDirectory "/drawable-hdpi")
+                                (/ 240 resolution))
+  ))
+
+(script-fu-register
+  "android-save-all-resolutions"         ; func name
+  "Save all resolutions"              ; menu label
+  "Saves all images (as 9-patch) into the\
+   'drawable-*' directories scaled and 9patched\
+   according to the current image's resolution"
+  "Greyson Fischer"                 ; author
+  "copyright 2011, Greyson Fischer" ; copyright notice
+  "July 9, 2011"                    ; date created
+  "RGBA"                            ; image type that the script works on
+  SF-IMAGE  "Image" 0               ; - current image
+  SF-DRAWABLE "Drawable" 0          ; - current layer
+  SF-DIRNAME "Resources Directory" "/tmp/res" ; directory into which to place drawable-*
+  )
+(script-fu-menu-register "android-save-all-resolutions" "<Image>/Image/Android")
